67e1e6f19446f08ced161bbd96eac266d2363fb8
dmitrii <dmitry-orlov@hotmail.com>
2017-10-30 01:00:15 +0100

    Added gulp and fixed routes requirement

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -8,22 +8,27 @@ module.exports = (app, db) => {
     // route declaration
     // CREATE NEW USER
     app.post(`/${DOMAIN}/new`, (req, res) => { 
-
+        res.send('Still a todo...')
     });
     // GET USER BY ID
     app.get(`/${DOMAIN}/:userId`, (req,res)=>{
-
+        res.send('Still a todo...')
     });
     // GET USER ADDED (LEARNING) WORDS
     app.get(`/${DOMAIN}/:userId/dictionary`, (req,res)=>{
-
+        res.send('Still a todo...')
     });
     // GET RELATIVE USER STATS
     app.get(`/${DOMAIN}/:userId/statistics`, (req,res)=>{
-
+        res.send('Still a todo...')
     })
-    // GET ALL USER STATS
-    app.get(`/${DOMAIN}/all/statistcs`, (req, res)=>{
-
+    // // GET ALL USER STATS
+    // app.get(`/${DOMAIN}/all/statistcs`, (req, res)=>{
+    //     console.log(req);
+    //     res.send('Still a todo...')
+    // });
+    app.get('/users/all/statistcs', (req, res)=>{
+        console.log(req);
+        res.send('Still a todo...')
     });
 };
