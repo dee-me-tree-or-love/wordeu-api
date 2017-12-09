be9139c0bc66e83e289cb90ad8938a480983be46
dmitrii <dmitry-orlov@hotmail.com>
2017-12-09 13:29:38 +0100

    added a dummy index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const wordRoutes = require('./word_routes.js');
 // the call to the routes index function
 module.exports = {
   config: (app, db, controllers) => {
+    // TODO: change to self describing endpoint here
+    app.get('/', (req, res) => res.send('What brings you here?'));
     userRoutes(app, db, controllers);
     wordRoutes(app, db, controllers);
   }
